ce644c72ca695b33c8f56ba77dd71c20e64d9cff
unknown <kodandareddy21@gmail.com>
2022-05-31 11:04:40 +0530

    Day 7

---
+++ Day7/index.js
@@ -0,0 +1,35 @@
+// // variable dec
+
+// // var,let,const
+// var fullName="Kodanda" 
+// //  full-name,
+// //  full_name,
+// //  FullName
+// var courseName="React Js Bootcamp"
+// var isLogin=true
+// var loginCount
+
+// loginCount=2
+// console.log(loginCount)
+// var paymentMethod
+
+// paymentMethod="Credit card"
+
+// console.log(paymentMethod)
+// var and const
+
+// const a=1
+// a=2
+//    console.log(a)
+
+const uniqueId="123456"
+var fullName="Kodanda Rama"
+var email="KodandaRama21@gmail.com"
+var password="112345"
+var conPassword="112345"
+var isLoginFromGoogle=false
+
+console.log(`my unique Id is ${uniqueId}
+     User Name is ${fullName}
+     UserEmail is ${email}
+`)
