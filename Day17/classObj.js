a188285f93b2832c0ba4a75943ed6617f51fd057
kodanda reddy <kodandareddy21@gmail.com>
2022-06-12 10:49:14 +0530

    Day18

--- Day17/classObj.js
+++ Day17/classObj.js
@@ -3,18 +3,34 @@ class User{
      this.name=name;
      this.email=email
     }
-    courseList=[]
-
+    #courseList=[]
+    getDetails(){
+        return `${this.name} and email ${this.email}`
+    }
     enroll(courseName){
-        this.courseList.push(courseName)
+        this.#courseList.push(courseName)
 
     }
     getCourseList(){
-        return this.courseList.length
+        return this.#courseList.length
     }
     login(){
         console.log("your are a user")
     }
 }
 
-export default User
+
+class SubAdmin extends User{
+    constructor(name,email){
+     super(name,email)
+    }
+    login(){
+        console.log("your are a sub admin")
+    }
+}
+let sub1=new SubAdmin("ramu","ramu@gamil.com")
+let user=new User("shyam","shyam@gmail.com")
+console.log(sub1,user)
+sub1.login()//method overriding
+user.login()
+// export default User
