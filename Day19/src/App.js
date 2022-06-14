ee7e30369712b29a4f003f25f5166ef0315db189
kodanda reddy <kodandareddy21@gmail.com>
2022-06-14 21:07:36 +0530

    React base temp

---
+++ Day19/src/App.js
@@ -0,0 +1,18 @@
+import Button from "./Button";
+import "./index.css";
+const App = () => {
+  return (
+    <div>
+      <Button name="submit"  value="11"/>
+      <Button name="submit1" />
+      <Button name="submit2"/>
+      <Button name="submit3"/>
+      <Button name="submit"  value="11"/>
+      <Button name="submit1" />
+      <Button name="submit2"/>
+      <Button name="submit3"/>
+    </div>
+  );
+};
+
+export default App;
---
+++ Day19/src/Button.js
@@ -0,0 +1,9 @@
+const Button=(props)=>{
+    console.log
+    return(
+        <button>{props.name}</button>
+    )
+}
+
+
+export default Button
